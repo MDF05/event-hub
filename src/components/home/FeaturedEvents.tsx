8cf63e2de329f41a50b901552dc938ae4592eec0
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2025-06-20 01:06:25 +0700

    feat: data update status

--- src/components/home/FeaturedEvents.tsx
+++ src/components/home/FeaturedEvents.tsx
@@ -75,6 +75,7 @@ export function FeaturedEvents() {
     );
   }
 
+  console.log("asyik");
   return (
     <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
       {events.map((event) => (
