b4646a29ed410f8dca4c887c08ad3e3875c65d93
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2025-06-20 02:14:12 +0700

    try: analyzing erro page '/'

--- src/components/home/FeaturedEvents.tsx
+++ src/components/home/FeaturedEvents.tsx
@@ -80,7 +80,7 @@ export function FeaturedEvents() {
  
   return (
     <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-      {events.map((event) => (
+      {(events || []).map((event) => (
         <EventCard key={event.id} event={event} />
       ))}
     </div>
